a953ea9367a3e2659b7ecd1d86ec777bdd0b716e
= <neo.ahlander@outlook.com>
2025-11-09 20:02:08 +0100

    Add FluentValidation and configure DI

--- src/Fundamenta.Api/Program.cs
+++ src/Fundamenta.Api/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Fundamenta.Api.Extensions;
 
 DotNetEnv.Env.Load();
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.ConfigureJsonSerializer();
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
